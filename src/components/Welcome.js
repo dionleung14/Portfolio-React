fc59eb70f197098db1c188835825f6d2dd25a10f
NA Flipper <dioncleung@gmail.com>
2020-07-21 16:38:12 -0700

    fixed sticky issue of welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,12 +3,14 @@ import React from "react";
 export default function Welcome() {
   return (
     <div className="w-8/12 mx-auto border border-black border-2 px-4">
-      <div className="border border-white bg-blue-400 bg-opacity-50 sticky top-0 mt-20 pt-10 pb-10">
+      {/* <div className="border border-white bg-blue-400 bg-opacity-50 sticky top-0 mt-20 mb-2 pt-10 pb-10">
         <h1 className="text-center text-2xl">hello</h1>
+      </div> */}
+      <div className="border border-red-500 bg-red-400 sticky top-0 pt-20">
+        <h1 className="text-center text-4xl top-0 mb-10 pt-10">
+          This should be the welcome area
+        </h1>
       </div>
-      <h1 className="border border-red-500 bg-red-400 text-center text-4xl sticky top-0 mb-10 pt-10">
-        This should be the welcome area
-      </h1>
       <br />
       <br />
       <br />
