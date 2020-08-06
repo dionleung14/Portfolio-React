800be8e08401d0f6cd3aa33ff493c006f3657146
NA Flipper <dioncleung@gmail.com>
2020-08-05 21:52:35 -0700

    layout for 3 testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,7 +5,17 @@ export default function Testimonials() {
   return (
     <div className="w-8/12 mx-auto border border-black border-2 px-4">
       <FirstHeader text="Testimonials" />
-      <h1 className="text-center sticky top-0">Testimonials</h1>
+      <div className="flex items-center justify-around border-white border-2 px-6 py-12">
+        <div>
+          <h1 className="text-center top-0">Testimonial 1</h1>
+        </div>
+        <div>
+          <h1 className="text-center top-0">Testimonial 2</h1>
+        </div>
+        <div>
+          <h1 className="text-center top-0">Testimonial 3</h1>
+        </div>
+      </div>
     </div>
   );
 }
