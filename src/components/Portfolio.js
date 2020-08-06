1c8fb7dfbe6b8a55f4e5905d774f2b31e219622e
NA Flipper <dioncleung@gmail.com>
2020-08-05 21:28:14 -0700

    layout for portfolio, work in progress

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,9 +5,29 @@ export default function Portfolio() {
   return (
     <div className="w-8/12 mx-auto border border-black border-2 px-4">
       <FirstHeader text="Portfolio, selected works" />
-      <h1 className="text-center sticky top-0">
-        This is supposed to be my portfolio
-      </h1>
+      <br />
+      <h1>Text for portfolio here?</h1>
+      <div className="flex items-center justify-around">
+        <div>
+          <img
+            className="w-1/6 border border-red-700 border-2 mr-8"
+            src="https://placekitten.com/800/1200"
+            alt="headshot"
+          />
+          <h3>Caption for this portfollio item</h3>
+        </div>
+        <img
+          className="w-1/6 border border-red-700 border-2 mr-8"
+          src="https://placekitten.com/800/1200"
+          alt="headshot"
+        />
+        <img
+          className="w-1/6 border border-red-700 border-2 mr-8"
+          src="https://placekitten.com/800/1200"
+          alt="headshot"
+        />
+      </div>
+      <h1 className="text-center top-0 mb-8">Text for portfolio here?</h1>
     </div>
   );
 }
